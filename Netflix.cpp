7a7742ec575cdea1ce19d48e452a92bddca31782
hzerrweckk <141746251+hzerrweckk@users.noreply.github.com>
2024-11-27 19:04:09 +0000

    aa

--- Netflix.cpp
+++ Netflix.cpp
@@ -52,7 +52,7 @@ int main(){
         switch (opcion) {
         case 1: {
             cout << "Cargando archivo de datos...\n";
-            ifstream archivo("datos.txt");
+            ifstream archivo("Datos.txt");
             if (!archivo.is_open()) {
                 cout << "Error al abrir el archivo de datos.\n";
                 break;
